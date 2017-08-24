ffb09893d2a0346a92e087723a9e1cf2a2be014a
bradcgarner <brad@bradgarner.com>
2017-08-24 16:21:05 -0400

    finished adding beer to list

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
 'use strict';
 
-// users enter items they need by entering text 
-//       in html: '.js-shopping-list-entry'
-let itemEntered = $('.js-shopping-list-entry').val();
-// and hit return or add
-//       add button html: $('.js-shopping-list-form button').submit  ...or .on('submit'...)
-$('.js-shopping-list-form button').submit(function(event){  
-  newItem();
-})
-//       return in html: .on('keypress')   ... event.which === 13   ... possibly keydown
-$('.js-shopping-list-entry').on('keypress', function(event){
-  newItem();
-}
-// check and uncheck items on the list via check button
-//       in html: $(.'shopping-item-toggle')
-$('.shopping-item-toggle').on('click', function(event){
-  $(this).find('.button-label').text(); // we ONLY want to change the label of the child of the button that was clicked...
-})
-// permently remove items from the list
-//       in html: $(.'shopping-item-delete')
-$('.shopping-list').on('click', '.shopping-item-delete', function(event){
+// and hit return or add: $('.js-shopping-list-form button').submit  ...or .on('submit'...)
+$('#js-shopping-list-form').on('submit', function (event) {
+  event.preventDefault();
+  newItem($('.js-shopping-list-entry').val());
+  $('.js-shopping-list-entry').val('');
+});
+
+// check and uncheck items on the list via check button... in html: $(.'shopping-item-toggle')
+$('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+  $(this).children().toggleClass('shopping-item__checked'); // we ONLY want to change the label of the child of the button that was clicked...
+  $(this).parent().parent().find('.shopping-item').toggleClass('shopping-item__checked');
+});
+// permently remove items from the list... in html: $(.'shopping-item-delete')
+$('.shopping-list').on('click', '.shopping-item-delete', function (event) {
   $(this).parent().parent().remove(); // permanently remove the li from the DOM
-})
+});
 
-
-function newItem(){
-// example of 1 li to add.
-<li>
-<span class="shopping-item">bread</span>
-<div class="shopping-item-controls">
-  <button class="shopping-item-toggle">
-    <span class="button-label">check</span>
-  </button>
-  <button class="shopping-item-delete">
-    <span class="button-label">delete</span>
-  </button>
-</div>
-</li>
+function newItem(item) {
+  let newHTML =
+    `<li>
+    <span class="shopping-item">${item}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
+  // add newHTML to the end of .'shopping-list'  
+  $('.shopping-list').append(newHTML);
 }
