4a3649b83734fbc1f5e7f1e6e9d8bfb1be0349fc
bradcgarner <brad@bradgarner.com>
2017-08-25 12:57:06 -0400

    got thru rendering shopping list

--- index2.js
+++ index2.js
@@ -1,15 +1,42 @@
 'use strict';
 // store the data in a JS array- not the DOM
-const STORE = [];
+const STORE = [
+  {name:'apple',id:1,checked:true},
+  {name:'banana',id:2,checked:false},
+  {name:'peach',id:'x',checked:true}
+];
 // objects with properties: ID, name, checked
+
+// create HTML for a single li within the ul
+function newItemHtml(item) {
+  let checkedClass = item.checked === true ? 'shopping-item__checked' : '';
+  let newHtml =
+    `
+    <li data-list-item="${item.id}">
+    <span class="shopping-item ${checkedClass}">${item.name}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label ${checkedClass}">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
+  return newHtml;
+}
 
 // shopping list should be rendered to the page
 function renderShoppingList() {
   // read the list from STORE
-  // create HTML from that list
-    // HTML will include name of item, use index number in array,
+    // for each item in the STORE array, run newItemHtml to get the li HTML
+    let htmlString = STORE.map(newItemHtml).join('');
+    // HTML will include name of item,id number in array,
     // and the appropriate class for check or not
-  // print HTML to the screen
+    // convert the array to a string
+    // print HTML to the screen
+    console.log(htmlString);
+    //$('.shopping-list').html(htmlString);
 }
 
 
@@ -60,8 +87,10 @@ function handleDeleteListItems() {
 // on Document Ready, render the shopping list and add event listeners to DOM
 function handleShoppingList() {
   renderShoppingList();
-  handleAddListItems();
-  handleCheckListItems();
-  handleDeleteListItems();
+  //handleAddListItems();
+  //handleCheckListItems();
+  //handleDeleteListItems();
 }
 
+//wrap in $() later
+handleShoppingList();
