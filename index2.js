3d3df3115f4e95d544743335f1b153cfc74b5a83
Benjamin Malin <benjaminamalin@gmail.com>
2017-08-25 15:34:54 -0400

    SO FRUSTRATING

--- index2.js
+++ index2.js
@@ -1,9 +1,9 @@
 'use strict';
 // store the data in a JS array- not the DOM
 const STORE = [
-  {name:'apple',id:1,checked:true},
-  {name:'banana',id:2,checked:false},
-  {name:'peach',id:'x',checked:true}
+  { name: 'apple', id: 1, checked: true },
+  { name: 'banana', id: 2, checked: false },
+  { name: 'peach', id: 3, checked: true }
 ];
 // objects with properties: ID, name, checked
 
@@ -29,32 +29,44 @@ function newItemHtml(item) {
 // shopping list should be rendered to the page
 function renderShoppingList() {
   // read the list from STORE
-    // for each item in the STORE array, run newItemHtml to get the li HTML
-    let htmlString = STORE.map(newItemHtml).join('');
-    // HTML will include name of item,id number in array,
-    // and the appropriate class for check or not
-    // convert the array to a string
-    // print HTML to the screen
-    console.log(htmlString);
-    //$('.shopping-list').html(htmlString);
+  // for each item in the STORE array, run newItemHtml to get the li HTML
+  let htmlString = STORE.map(newItemHtml).join('');
+  // HTML will include name of item,id number in array,
+  // and the appropriate class for check or not
+  // convert the array to a string
+  // print HTML to the screen
+  //$('.shopping-list').html(htmlString);
+  $('.shopping-list').html(htmlString);
+  $('.js-shopping-list-entry').val('');
 }
 
 
 // add items to the list *****
-function addListItems(id) {
+function addListItems(name) {
   // read the user-submitted text (text box in the form)
   // add that text with an ID and with a status to STORE
   // ********* ID will be the id of the last item in STORE
-  let id = STORE.length === 0 ? 1 : STORE[STORE.length-1].ID+1 ;
+  let id = STORE.length === 0 ? 1 : STORE[STORE.length - 1].id + 1;
+  STORE.push({
+    name,
+    id,
+    checked: false
+  });
+  console.log(STORE);
+  renderShoppingList();
   // ask TA about where / how to do id generation
 }
 
 
 // check items off the list *****
 function checkListItems(id) {
+  console.log(id);
   // get the ID # of which item to check off
   // apply a checked class to the item 
+  STORE.find(x => x.id === id).checked = STORE.find(x => x.id === id).checked === false ? true : false;
   // update the status property in STORE to 'checked' to true
+  renderShoppingList();
+  console.log(STORE);
 }
 
 
@@ -72,6 +84,10 @@ function deleteListItems(id) {
 function handleAddListItems() {
   // use jQuery to find the Submit button in the DOM
   // assign addListItems to the Submit button .on('click')
+  $('#js-shopping-list-form').on('submit', function (event) {
+    event.preventDefault();
+    addListItems($('.js-shopping-list-entry').val());
+  });
 }
 
 
@@ -79,6 +95,10 @@ function handleAddListItems() {
 function handleCheckListItems() {
   // use jQuery to find the Check button in the DOM
   // assign checkListItems to the Check button .on('click')
+  $('.shopping-item-toggle').on('click', function (event) {
+    console.log($(this).closest('li').data('list-item'));
+  $('.shopping-item-toggle').on('click', checkListItems($(this).closest('li').data('list-item')));
+  });
 }
 
 
@@ -92,8 +112,8 @@ function handleDeleteListItems() {
 // on Document Ready, render the shopping list and add event listeners to DOM
 function handleShoppingList() {
   renderShoppingList();
-  //handleAddListItems();
-  //handleCheckListItems();
+  handleAddListItems();
+  handleCheckListItems();
   //handleDeleteListItems();
 }
 
